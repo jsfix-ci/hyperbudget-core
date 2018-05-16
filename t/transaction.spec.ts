7ff5e2e2493ee5de4b38dc9b0a7aa6bf51327ab1
Errietta Kostala <errietta@errietta.me>
2018-05-16 17:28:17 +0100

    tests for transaction

--- t/transaction.spec.ts
+++ t/transaction.spec.ts
@@ -2,6 +2,7 @@
 import { Transaction } from '../src/lib/transaction';
 import { expect, assert } from 'chai';
 import 'mocha';
+import moment from 'moment';
 
 import { SystemConfig } from '../src/lib/config/system';
 
@@ -44,5 +45,47 @@ describe('Transaction', () => {
     });
     expect(transaction.month).to.equal('201702');
   });
-  
+  it('parses dates', () => {
+    let transaction = new Transaction({
+      txn_date: '05/07/2018',
+      txn_type: 'DEB',
+      txn_amount_credit: 0,
+      txn_amount_debit: 25,
+      acc_balance: 0,
+      txn_desc: 'Amazon UK',
+      txn_src: 'lloyds',
+    });
+
+    expect(transaction.txn_date).to.be.instanceOf(Date);
+    expect(moment(transaction.txn_date).isSame(moment('2018-07-05'))).to.be.true;
+
+    transaction = new Transaction({
+      txn_date: '2018-03-01',
+      txn_type: 'DEB',
+      txn_amount_credit: 0,
+      txn_amount_debit: 25,
+      acc_balance: 0,
+      txn_desc: 'Amazon UK',
+      txn_src: 'fairfx-corp',
+    });
+
+    expect(transaction.txn_date).to.be.instanceOf(Date);
+    expect(moment(transaction.txn_date).isSame(moment('2018-03-01'))).to.be.true;
+  });
+
+  it('number-fies amounts', () => {
+    let transaction = new Transaction({
+      txn_date: '05/07/2018',
+      txn_type: 'DEB',
+      txn_amount_credit: 10,
+      txn_amount_debit: 25,
+      acc_balance: 0,
+      txn_desc: 'Amazon UK',
+    });
+
+    expect(typeof transaction.txn_amount_debit).to.equal('number');
+    expect(typeof transaction.txn_amount_credit).to.equal('number');
+    expect(transaction.txn_amount_credit).to.equal(10);
+    expect(transaction.txn_amount_debit).to.equal(25);
+  });
 });
