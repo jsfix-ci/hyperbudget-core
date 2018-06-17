2616478ee5071f21e81516f364a59124a1bc9998
Errietta Kostala <errietta@errietta.me>
2018-06-18 00:00:34 +0100

    Export this too

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,5 @@ export { ReportManager } from './lib/manager/reportmanager';
 
 export { FormattedTransaction } from './types/formatted-transaction';
 export { CategoryAmounts } from './types/category-amounts';
+
+export * from './lib/validator/category';
